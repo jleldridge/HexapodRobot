5a85b019723f6a6cfe6c8c87cdd16f0fd1e6bb19
Luke Eldridge <jleldridge27@gmail.com>
2015-11-18 12:55:34 -0500

    Added manufacturer walk code for reference.

--- Hexapod.ino
+++ Hexapod.ino
@@ -32,28 +32,10 @@ int servos[] = { 0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13 };
 int servoCount = 12;
 
 void setup() {
-	Serial.begin(9600);
-	Serial.println("16 channel Servo test!");
-
+	Serial.begin(9600);	
 	pwm.begin();
-
-	pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-}
-
-// you can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
-void setServoPulse(uint8_t n, double pulse) {
-	double pulselength;
-
-	pulselength = 1000000;   // 1,000,000 us per second
-	pulselength /= 60;   // 60 Hz
-	Serial.print(pulselength); Serial.println(" us per period");
-	pulselength /= 4096;  // 12 bits of resolution
-	Serial.print(pulselength); Serial.println(" us per bit");
-	pulse *= 1000;
-	pulse /= pulselength;
-	Serial.println(pulse);
-	pwm.setPWM(n, 0, pulse);
+	pwm.setPWMFreq(60);
+	Serial.println("Initialized...");
 }
 
 void loop() {
@@ -61,8 +43,8 @@ void loop() {
 	delay(2000);
 	maxServos();
 	delay(2000);
-	minServos();
-	delay(2000);
+	//minServos();
+	//delay(2000);
 }
 
 void centerServos() {
